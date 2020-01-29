4a85407ebf3fac2745822c32afdc02766c0fee54
Hunter@pi <ihunt0002@gmail.com>
2020-01-29 23:16:52 +0000

    Adding bot.py

---
+++ bot.py
@@ -0,0 +1,33 @@
+# bot.py
+
+import os
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+description = '''M64n't in Python'''
+bot = commands.Bot(command_prefix='!', description=description)
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+
+@bot.command()
+async def hello(ctx):
+    """Says world"""
+    await ctx.send("world")
+
+
+@bot.command()
+async def add(ctx, left : int, right : int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+
+bot.run(TOKEN)

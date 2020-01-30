a608584354ebd12aac2960e4ece5c01128178030
HunterM64 <ihunt0002@gmail.com>
2020-01-30 10:19:00 -0500

    testing bot commands and events conflicts

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-description = '''Your M64 away from M64'''
+description = '''Your lord and saviour, M64n't!'''
 bot = commands.Bot(command_prefix='!', description=description)
  
 @bot.event
@@ -30,17 +30,21 @@ async def add(ctx, left : int, right : int):
     """Adds two numbers together."""
     await ctx.send(left + right)
 
+@bot.command()
+async def foo(ctx):
+    """just testing bot event and command conflicts"""
+    await ctx.send("did i pass the test?")
+    
 # bot events
 @bot.event
 async def on_message(message):
-    if ("foo" in message.content) and (message.author.bot == False):
-        await message.channel.send("foo to you too")
-    await bot.process_commands(message)
-    
-@bot.event
-async def on_message(message):
+
     if ("owo" in message.content) and (message.author.bot == False):
         await message.channel.send("don't you freaking owo me")
+
+    if ("foo" in message.content) and (message.author.bot == False):
+        await message.channel.send("foo to you too!")
+        
     await bot.process_commands(message)
     
 

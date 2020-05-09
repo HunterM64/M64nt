cf113c45f77a76f012a833dd78dbf22e5db82b97
hunter@Roxanne <ihunt0002@gmail.com>
2020-05-09 11:00:52 -0400

    updating

--- cogs/user.py
+++ cogs/user.py
@@ -11,37 +11,17 @@ class User(commands.Cog):
         self.bot = bot
         self._last_member = None
 
-    #commands
-    @commands.command()
-    async def hello(self, ctx):
-        """Says world"""
-        await ctx.send("world")
-
-
-    @commands.command()
-    async def add(self, ctx, left : int, right : int):
-        """Adds two numbers together."""
-        await ctx.send(left + right)
+    # commands
 
     @commands.command()
     async def ping(self, ctx):
         """Tests bot latency"""
         await ctx.send("pong! {0} ms".format(round(self.bot.latency, 3) * 1000))
-
-    @commands.command()
-    async def dm(self, ctx):
-        """Sends a DM"""
-        await ctx.author.send("whomst has summoned the ancient one?")
-
-    @commands.command()
-    async def wsl(self, ctx):
-        """Did you know about WSL?"""
-        await ctx.send("Did you know WSL lets you use a Bash terminal on Windows? Epic!")
                                 
     @commands.command()
     async def uptime(self, ctx):
         """Shows bot uptime"""
-        now = datetime.datetime.utcnow() #Timestamp of when uptime function is run
+        now = datetime.datetime.utcnow() # Timestamp of when uptime function is run
         delta = now - start_time
         hours, remainder = divmod(int(delta.total_seconds()), 3600)
         minutes, seconds = divmod(remainder, 60)
@@ -52,3 +32,25 @@ class User(commands.Cog):
             time_format = "**{h}** hours, **{m}** minutes, and **{s}** seconds."
         uptime_stamp = time_format.format(d=days, h=hours, m=minutes, s=seconds)
         await ctx.send("I have been up for {}".format(uptime_stamp))
+
+    # old commands 
+
+    # @commands.command()
+    # async def hello(self, ctx):
+    #     """Says world"""
+    #     await ctx.send("world")
+
+    # @commands.command()
+    # async def add(self, ctx, left : int, right : int):
+    #     """Adds two numbers together."""
+    #     await ctx.send(left + right)
+
+    # @commands.command()
+    # async def dm(self, ctx):
+    #     """Sends a DM"""
+    #     await ctx.author.send("whomst has summoned the ancient one?")
+
+    # @commands.command()
+    # async def wsl(self, ctx):
+    #     """Did you know about WSL?"""
+    #     await ctx.send("Did you know WSL lets you use a Bash terminal on Windows? Epic!")

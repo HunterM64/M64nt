43e13427614b4d196de39b45d56416292fd8a389
DebianHunter <ihunt0002@gmail.com>
2020-05-18 10:50:12 -0400

    added function finsSimilarString

--- bot.py
+++ bot.py
@@ -37,11 +37,33 @@ async def on_message(message):
         
     await bot.process_commands(message)
 
+# functions 
+
+# finds command most similar to command given 
+def findSimilarString(string):
+    returnString = ""
+    commands = [ "help", "uptime", "ping"]
+    overlap = 0
+    bestOverlap = 0
+    bestString = ""
+    i = 0
+
+    for testString in commands:
+        while( i < len(string)):
+            if(string[i] == testString[i]):
+                overlap += 1
+            i +=1
+        if (overlap > bestOverlap):
+            bestString = testString
+    
+    returnString = bestString
+    return returnString
+
 # error bot event
 @bot.event
 async def on_command_error(ctx, error):
     if isinstance(error, CommandNotFound):
-        await ctx.send("Command not found, did you mean?")
+        await ctx.send("Command not found, did you mean" + findSimilarString("helo"))
     else:
         await ctx.send(error)
 
